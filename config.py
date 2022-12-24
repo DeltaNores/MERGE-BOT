0345519ef6b227c5983ba7243fb9495155301193
DeltaNores <121076265+DeltaNores@users.noreply.github.com>
2022-12-24 03:47:47 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 class Config(object):
     API_HASH = os.environ.get("424529da270fe76f271bab41f52721b6")
     BOT_TOKEN = os.environ.get("5888658466:AAGV30meoGueIImdwqgzo216AqLB02gYV5Q")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    TELEGRAM_API = os.environ["21158798"]
+    OWNER = os.environ.get("5975310295")
+    OWNER_USERNAME = os.environ.get("mievilo98")
+    PASSWORD = os.environ.get("KirKhar")
+    DATABASE_URL = os.environ.get("mongodb+srv://Merge:N0Ra#25_@@cluster0.ixvao.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("-1001825431724")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    USER_SESSION_STRING = os.environ.get("mievilo98", None)
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
